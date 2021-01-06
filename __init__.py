ce3440b600e76e583aadc30a5b0ff0bfb275fd44
srmonk <32929369+srmonk@users.noreply.github.com>
2021-01-06 19:49:00 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,12 +5,7 @@ from mycroft.util.parse import extract_duration
 class MyFirstSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-
-    @intent_handler('skill.first.my.intent')
-    def handle_skill_first_my(self, message):
-        self.speak_dialog('skill.first.my')
-
-        
+   
         
     @intent_handler('skill.study.intent')
     def handle_skill_study(self, message):
