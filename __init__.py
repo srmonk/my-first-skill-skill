fb7f1723f035af23d57a50ef033ffc9f9ba15e73
srmonk <32929369+srmonk@users.noreply.github.com>
2021-01-06 19:42:05 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+import time
 from mycroft import MycroftSkill, intent_handler
 from mycroft.util.parse import extract_duration
 
@@ -9,10 +10,26 @@ class MyFirstSkill(MycroftSkill):
     def handle_skill_first_my(self, message):
         self.speak_dialog('skill.first.my')
 
+        
+        
     @intent_handler('skill.study.intent')
     def handle_skill_study(self, message):
         time = extract_duration(self.get_response('skill.study'))[0]
         self.speak_dialog('skill.study.confirmation', {'time': str(time)})
+        
+        blocks = extract_duration(self.get_response('skill.study'))[0]
+        self.speak_dialog('skill.study.confirmation', {'time': str(t)})
+        blocks = int(blocks)
+        for i in range(blocks):
+            time.sleep(1500)
+            if i <  blocks-1:
+                self.speak_dialog('skill.study.break.begin')
+                time.sleep(300)
+                self.speak_dialog('skill.study.break.end') 
+            if i == blocks-1:
+                break
+        self.speak_dialog('skill.study.end') 
+
 
 
 def create_skill():
